7d4ba3b28608ab64a163865f6d6e70f9b714e881
ShimonakaRina <sixseasonrain11614@gmail.com>
2021-06-14 07:56:55 +0000

    4-3finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3bfc9064aaf9969a2bc1ed70a972eb044ef9a6a48c3cb0be95a38b86f820f44c3ba91c71f31c5e122f2774186f9d367b054b169885bd2729e6f6eb72c2b9aa37'
